3d7672fb3daa2759e77037069efd0d095ba21c9f
Sarah Lindmar <srhcrete@gmail.com>
2017-12-19 01:24:24 -0800

    pass association testing for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ApplicationRecord
-  belongs_to :bands
+  belongs_to :band
   belongs_to :service
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-# RSpec.describe Review, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+describe Review, 'association' do
+  it { should belong_to :band }
+  it { should belong_to :service }
+end
