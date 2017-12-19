2b5f376cbfc88a2e2ea657548d3f7829ebc6fa50
Sarah Lindmar <srhcrete@gmail.com>
2017-12-19 02:17:02 -0800

    add coordinates to services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219084419) do
+ActiveRecord::Schema.define(version: 20171219101134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20171219084419) do
     t.datetime "available_to"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
