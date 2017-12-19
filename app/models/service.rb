1c622353242a478e2af136349a675a8c35669ab3
Sarah Lindmar <srhcrete@gmail.com>
2017-12-19 15:21:32 -0800

    update service model with image validations

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,7 @@ class Service < ApplicationRecord
   has_many :bookings
   has_many :tags, as: :tagable
   has_many :reviews
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
