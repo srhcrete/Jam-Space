bfea62ffb4cddc77f217309cf084abfd2da93ee5
Guest User <Guest@Epicodus-2E.local>
2017-12-21 16:22:31 -0800

    add bcrypt user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
 
+
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      User.find { |u| u.id == session[:user_id] }
+    else
+      nil
+    end
   end
+
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  attr_accessor :password
+  before_save :encrypt_password
 
   has_many :bands
   has_many :services
@@ -15,4 +19,28 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { medium: "100x100>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def encrypt_password
+    if password_digest.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_digest = BCrypt::Engine.hash_secret(password_digest, password_salt)
+    end
+  end
+
+  def self.authenticate(email, password_digest)
+    user = User.find_by_email(email)
+    if user && user.password_digest == BCrypt::Engine.hash_secret(password_digest, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  # def encrypt_password
+  #   self.encrypted_password = BCrypt::Password.create(password) if password.present?
+  # end
+  #
+  # def match_password(login_password="")
+  #   BCrypt::Password.new(password) == login_password
+  # end
 end
